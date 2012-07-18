76287ccf4a8c5c2c309950979e5e92170eb7370a
pdq <idkbot@gmail.com>
2012-07-17 22:13:33 -0500

    updated awesomewm-X to current

--- backup.sh
+++ backup.sh
@@ -11,7 +11,7 @@ bldgreen=${txtbld}$(tput setaf 2) # Green Colored
 bldblue=${txtbld}$(tput setaf 6) # Blue Colored
 bldyellow=${txtbld}$(tput setaf 3) # Yellow Colored
 txtrst=$(tput sgr0)             # Reset
-commit_msg=$argv[0]
+commit_msg=$1
 
 if [ `id -u` -eq 0 ]; then
    echo "${bldred}Do not run me as root!${txtrst} =)"
@@ -27,10 +27,10 @@ cd ${my_home}.config
 cp -r awesome/* ${my_home}${dev_directory}/awesomewm-X
 cd ${my_home}${dev_directory}/awesomewm-X
 git add .
-if [ ! $commit_msg ]; then 
-	$commit_msg = 'updated awesomewm-X to current'
+if [ "$commit_msg" == "" ]; then
+	commit_msg='updated awesomewm-X to current'
 fi
-git commit -m $commit_msg
+git commit -m "$commit_msg"
 git push origin master
 echo "
 
@@ -45,10 +45,10 @@ cd ${my_home}.config
 cp -r conky/* ${my_home}${dev_directory}/conky-X
 cd ${my_home}${dev_directory}/conky-X
 git add .
-if [ ! $commit_msg ]; then 
-	$commit_msg = 'updated conky-X to current'
+if [ "$commit_msg" == "" ]; then
+	commit_msg='updated conky-X to current'
 fi
-git commit -m $commit_msg
+git commit -m "$commit_msg"
 git push origin master
 echo "
 
@@ -63,10 +63,10 @@ cd ${my_home}.config
 cp -r luakit/* ${my_home}${dev_directory}/luakit-X
 cd ${my_home}${dev_directory}/luakit-X
 git add .
-if [ ! $commit_msg ]; then 
-	$commit_msg = 'updated luakit-X to current'
+if [ "$commit_msg" == "" ]; then
+	commit_msg='updated luakit-X to current'
 fi
-git commit -m $commit_msg
+git commit -m "$commit_msg"
 git push origin master
 echo "
 
@@ -98,9 +98,8 @@ echo "${bldgreen} ==> copied /etc/ files into ${dotfiles}${txtrst}
 sleep 2s
 
 echo "## Create main.lst remove local, base
-pacman -Qqe | grep -vx "$(pacman -Qqg base)" | grep -vx "$(pacman -Qqm)" > main.lst
 ## Create local.lst of local (includes AUR) packages installed
-pacman -Qqm > local.lst"
+"
 
 sleep 2s
 # dotfiles repo
@@ -111,10 +110,10 @@ pacman -Qqe | grep -vx "$(pacman -Qqg base)" | grep -vx "$(pacman -Qqm)" > main.
 pacman -Qqm > local.lst
 sleep 5s
 git add .
-if [ ! $commit_msg ]; then 
-	$commit_msg = 'updated packages lists from source'
+if [ "$commit_msg" == "" ]; then
+	commit_msg='updated packages lists from source'
 fi
-git commit -m $commit_msg
+git commit -m "$commit_msg"
 git push origin master
 echo "
 
