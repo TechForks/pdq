3103ece2d46b116ea3a4ea104c1cf3de049d234d
pdq <idkbot@gmail.com>
2012-07-29 06:31:10 -0500

    updated packages lists from source

--- reinstaller.sh
+++ reinstaller.sh
@@ -28,8 +28,8 @@ awesome_repo="git://github.com/idk/awesomewm-X.git"
 conky_repo="git://github.com/idk/conky-X.git"
 luakit_repo="git://github.com/idk/luakit-X.git"
 done_format="========================================="
-choice_count=11
-root_choice_count=6
+choice_count=9
+root_choice_count=5
 txtbld=$(tput bold)             # Bold
 bldred=${txtbld}$(tput setaf 1) # Red Colored
 bldgreen=${txtbld}$(tput setaf 2) # Green Colored
@@ -39,44 +39,44 @@ txtrst=$(tput sgr0)             # Reset
 
 if [ `id -u` -eq 0 ]; then
    function root_menulist() { 
+      # if [ $root_highlight -eq 1 ] ; then
+      #    echo "${bldgreen}1. Pacman-key initialization -- done!${txtrst}"
+      # else
+      #    echo "1. Pacman-key initialization"
+      # fi
       if [ $root_highlight -eq 1 ] ; then
-         echo "${bldgreen}1. Pacman-key initialization -- done!${txtrst}"
+         echo "${bldgreen}1. Create a USER and /home/USER and generate locale -- done!${txtrst}"
       else
-         echo "1. Pacman-key initialization"
+         echo "1. Create a USER and /home/USER and generate locale"
       fi
       if [ $root_highlight -eq 2 ] ; then
-         echo "${bldgreen}2. Create a USER and /home/USER and generate locale -- done!${txtrst}"
+         echo "${bldgreen}2. Edit /etc/pacman.conf${txtrst}"
       else
-         echo "2. Create a USER and /home/USER and generate locale"
+         echo "2. Edit /etc/pacman.conf"
       fi
       if [ $root_highlight -eq 3 ] ; then
-         echo "${bldgreen}3. Edit /etc/pacman.conf${txtrst}"
+         echo "${bldgreen}3. Add USER to sudoers -- done!${txtrst}"
       else
-         echo "3. Edit /etc/pacman.conf"
+         echo "3. Add USER to sudoers "
       fi
       if [ $root_highlight -eq 4 ] ; then
-         echo "${bldgreen}4. Add USER to sudoers -- done!${txtrst}"
+         echo "${bldgreen}4. Switch to your newly created USER${txtrst}" 
       else
-         echo "4. Add USER to sudoers "
+         echo "4. Switch to your newly created USER" 
       fi
       if [ $root_highlight -eq 5 ] ; then
-         echo "${bldgreen}5. Switch to your newly created USER${txtrst}" 
+         echo "${bldred}5. Exit root Installer${txtrst}"
       else
-         echo "5. Switch to your newly created USER" 
+         echo "5. Exit root Installer"
       fi
-      if [ $root_highlight -eq 6 ] ; then
-         echo "${bldred}6. Exit root Installer${txtrst}"
-      else
-         echo "6. Exit root Installer"
-      fi
-   echo -n "${bldred}Please choose [1,2,3,4,5 or 6 ? (HINT: start at 1)${txtrst}"
+   echo -n "${bldred}Please choose [1,2,3,4 or 5 ? (HINT: start at 1)${txtrst}"
 
       # Loop while the variable choice is equal $choice_count
    }
 
 # Declare variable choice and assign value $choice_count
    root_choice=$root_choice_count
-   root_highlight=11
+   root_highlight=6
    # Print to stdout
    #menulist
    # bash while loop
@@ -85,17 +85,17 @@ if [ `id -u` -eq 0 ]; then
       # read user input
       read root_choice
       # bash nested if/else
-       if [ $root_choice -eq 1 ] ; then
+         # if [ $root_choice -eq 1 ] ; then
          # echo "${bldgreen} ==> pacman-key initialization starting ...${txtrst}"
          # echo "HINT: Mash on the keyboard keys and move mouse to make entropy process go faster! :D"
          # sleep 5s
          # pacman-key --init
          # pacman-key --populate archlinux
-         echo "done ..."
-         root_choice=$root_choice_count
-         echo $done_format
-         root_highlight=1
-      elif [ $root_choice -eq 2 ] ; then
+         # echo "done ..."
+         # root_choice=$root_choice_count
+         # echo $done_format
+         # root_highlight=1
+      if [ $root_choice -eq 1 ] ; then
          echo "${bldblue} ==> Creating user...${txtrst}"
          sleep 3s
          read -p "Enter username : " username
@@ -109,9 +109,9 @@ if [ `id -u` -eq 0 ]; then
             [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
          fi
          root_choice=$root_choice_count
-         root_highlight=2
+         root_highlight=1
          echo $done_format
-      elif [ $root_choice -eq 3 ] ; then
+      elif [ $root_choice -eq 2 ] ; then
          echo "cp /etc/pacman.conf /etc/pacman.conf.orig"
          cp /etc/pacman.conf /etc/pacman.conf.orig
          echo "${bldgreen} ==> Adding repo to /etc/pacman.conf${txtrst}"
@@ -124,8 +124,8 @@ Server = http://repo.archlinux.fr/\$arch >> /etc/pacman.conf'"
          pacman -S yaourt fakeroot git
          root_choice=$root_choice_count
          echo $done_format
-         root_highlight=3
-      elif [ $root_choice -eq 4 ] ; then
+         root_highlight=2
+      elif [ $root_choice -eq 3 ] ; then
          echo "${bldgreen}Edit sudoers, UNCOMMENT wheel...${txtrst}
 
 ${bldblue}% wheel ALL=(ALL) ALL${txtrst}
@@ -136,8 +136,8 @@ ${bldblue}% wheel ALL=(ALL) ALL${txtrst}
          echo "done..."
          root_choice=$root_choice_count
          echo $done_format 
-         root_highlight=4
-      elif [ $root_choice -eq 5 ] ; then
+         root_highlight=3
+      elif [ $root_choice -eq 4 ] ; then
          echo "${bldgreen}Switch to USER${txtrst}"
          read -p "Username : " username
          egrep "^$username" /etc/passwd >/dev/null
@@ -154,20 +154,18 @@ ${bldblue}% wheel ALL=(ALL) ALL${txtrst}
          fi
          root_choice=$root_choice_count
          echo $done_format
-         root_highlight=5
-      elif [ $root_choice -eq 6 ] ; then
+         root_highlight=4
+      elif [ $root_choice -eq 5 ] ; then
          echo $done_format
          sleep 2s
-         root_highlight=6
+         root_highlight=5
          echo "${bldgreen} ==> Exiting root install script...${txtrst}"
-         sleep 3s
-         echo "${bldgreen}To start Awesome as user type: 'startx' ...${txtrst}"
          sleep 3s
          echo "${bldgreen}Re-Run install script: 'sh reinstaller.sh'${txtrst} | ${bldred}Remove install script 'rm reinstaller.sh'${txtrst}"
          sleep 3s
          exit
       else
-         echo "${bldred}Please make a choice between 1-6! (HINT: start at 1)${txtrst}"
+         echo "${bldred}Please make a choice between 1-5! (HINT: start at 1)${txtrst}"
          root_highlight=${root_highlight}
          root_menulist
          choice=$root_choice_count
@@ -176,69 +174,59 @@ ${bldblue}% wheel ALL=(ALL) ALL${txtrst}
 else
    function menulist() { 
       if [ $highlight -eq 1 ] ; then
-         echo "${bldgreen}1. Create ${my_home}${dotfiles} and ${my_home}vital/ -- done!${txtrst}"
+         echo "${bldgreen}1. Create ${my_home}${dotfiles} and ${my_home}vital and git clone backup to ${dotfiles} -- done!${txtrst}"
       else
-         echo "1. Create ${my_home}${dotfiles} and ${my_home}vital/"
+         echo "1. Create ${my_home}${dotfiles} and ${my_home}vital and git clone backup to ${dotfiles}"
       fi
       if [ $highlight -eq 2 ] ; then
-         echo "${bldgreen}2. Git clone backup to ${dotfiles}-- done!${txtrst}"
+         echo "${bldgreen}2. Backup various configs, mirrorlist and write/rank/sort new mirrorlist -- done!${txtrst}"
       else
-         echo "2. Git clone backup to ${dotfiles}"
+         echo "2. Backup various configs, mirrorlist and write/rank/sort new mirrorlist"
       fi
       if [ $highlight -eq 3 ] ; then
-         echo "${bldgreen}3. Backup mirrorlist and write/rank/sort new mirrorlist -- done!${txtrst}"
+         echo "${bldgreen}3. Install main packages -- done!${txtrst}" 
       else
-         echo "3. Backup mirrorlist and write/rank/sort new mirrorlist"
+         echo "3. Install main packages" 
       fi
       if [ $highlight -eq 4 ] ; then
-         echo "${bldgreen}4. Backup and copy root configs -- done!${txtrst}" 
+         echo "${bldgreen}4. Install AUR packages --noconfirm -- done!${txtrst}" 
       else
-         echo "4. Backup and copy root configs" 
+         echo "4. Install AUR packages --noconfirm" 
       fi
       if [ $highlight -eq 5 ] ; then
-         echo "${bldgreen}5. Install main packages -- done!${txtrst}" 
+         echo "${bldgreen}5. Install AUR packages  (If Step 5 fails) -- done!${txtrst}" 
       else
-         echo "5. Install main packages" 
+         echo "5. Install AUR packages  (If Step 5 fails)" 
       fi
       if [ $highlight -eq 6 ] ; then
-         echo "${bldgreen}6. Install AUR packages --noconfirm -- done!${txtrst}" 
+         echo "${bldgreen}6. Backup and copy user configs -- done!${txtrst}" 
       else
-         echo "6. Install AUR packages --noconfirm" 
-      fi
-      if [ $highlight -eq 7 ] ; then
-         echo "${bldgreen}7. Install AUR packages  (If Step 5 fails) -- done!${txtrst}" 
-      else
-         echo "7. Install AUR packages  (If Step 5 fails)" 
-      fi
-      if [ $highlight -eq 8 ] ; then
-         echo "${bldgreen}8. Backup and copy user configs -- done!${txtrst}" 
-      else
-         echo "8. Backup and copy user configs" 
+         echo "6. Backup and copy user configs" 
       fi
 
-      if [ $highlight -eq 9 ] ; then
-         echo "${bldgreen}9. Install Awesome Window Manager (awesomewm-X), luakit-X and Conky (conky-X) -- done!${txtrst}" 
+      if [ $highlight -eq 7 ] ; then
+         echo "${bldgreen}7. Install Awesome Window Manager (awesomewm-X), luakit-X and Conky (conky-X) -- done!${txtrst}" 
       else
-         echo "9. Install Awesome Window Manager (awesomewm-X), luakit-X and Conky (conky-X)" 
+         echo "7. Install Awesome Window Manager (awesomewm-X), luakit-X and Conky (conky-X)" 
       fi
-      if [ $highlight -eq 10 ] ; then
-         echo "${bldgreen}10. Install LAMPP (Apache/MySQL/PHP/PHPMyAdmin) -- done!${txtrst}" 
+      if [ $highlight -eq 8 ] ; then
+         echo "${bldgreen}8. Install LAMPP (Apache/MySQL/PHP/PHPMyAdmin) -- done!${txtrst}" 
       else
-         echo "10. Install LAMPP (Apache/MySQL/PHP/PHPMyAdmin)" 
+         echo "8. Install LAMPP (Apache/MySQL/PHP/PHPMyAdmin)" 
       fi
-      if [ $highlight -eq 11 ] ; then
-         echo "${bldred}11. Exit Installer${txtrst}"
+      if [ $highlight -eq 9] ; then
+         echo "${bldred}9. Exit Installer${txtrst}"
       else
-         echo "11. Exit Installer"
+         echo "9. Exit Installer"
       fi
-   echo -n "${bldred}Please choose [1,2,3,4,5,7,8,9,10 or 11] ? (HINT: start at 1)${txtrst}"
+   echo -n "${bldred}Please choose [1,2,3,4,5,7,8 or 9] ? (HINT: start at 1)${txtrst}"
 
       # Loop while the variable choice is equal $choice_count
    }
 
    # Declare variable choice and assign value $choice_count
    choice=$choice_count
-   highlight=12
+   highlight=10
    # Print to stdout
    #menulist
    # bash while loop
@@ -253,25 +241,17 @@ else
          mkdir ${dev_directory}
          cd ${my_home}${dev_directory}
          pwd
-         echo "${bldblue} ==> Creating ${my_home}vital/${txtrst}"
-         cd ${my_home}
-         mkdir vital
-         cd ${my_home}vital
-         pwd
+         sleep 1s
          echo "${bldblue} ==> Creating ${my_home}vital/pkg/${txtrst}"
-         cd ${my_home}vital
-         mkdir pkg
+         mkdir -p ${my_home}vital/pkg
          cd ${my_home}vital/pkg
          pwd
+         sleep 1s
          echo "${bldblue} ==> Creating ${my_home}vital/tmp/${txtrst}"
-         cd ${my_home}vital
-         mkdir tmp
+         mkdir -p ${my_home}vital/tmp
          cd ${my_home}vital/tmp
          pwd
-         choice=$choice_count
-         highlight=1
-         echo $done_format
-      elif [ $choice -eq 2 ] ; then
+         sleep 2s
          echo "${bldgreen} ==> Git clone backup to ${dotfiles}${txtrst}"
          ## my backups repo
          cd ${my_home}${dev_directory}
@@ -281,19 +261,16 @@ else
          ls --color=auto -a
          choice=$choice_count
          echo $done_format
-         highlight=2
-      elif [ $choice -eq 3 ] ; then
+         highlight=1
+      elif [ $choice -eq 2 ] ; then
          echo "${bldgreen} ==> Backing up mirrorlist and write/rank/sort new mirrorlist${txtrst}"
          sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
          sudo cp ${my_home}${dotfiles}etc/mirrorlist /etc/pacman.d/mirrorlist
          echo "sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo cp ${my_home}${dotfiles}etc/mirrorlist /etc/pacman.d/mirrorlist"
-         choice=$choice_count
-         echo $done_format 
-         highlight=3
-      elif [ $choice -eq 4 ] ; then
-         echo "${bldgreen} ==> Backing up and copying root configs${txtrst}"
          sleep 2s
+         echo "${bldgreen} ==> Backing up and copying root configs${txtrst}"
+         sleep 1s
          sudo mv /etc/rc.conf /etc/rc.conf.bak
          sudo cp ${my_home}${dotfiles}etc/rc.conf /etc/rc.conf
          sudo mv /etc/pacman.conf /etc/pacman.conf.bak
@@ -312,30 +289,30 @@ sudo cp ${my_home}${dotfiles}etc/custom.conf /etc/X11/xorg.conf.d/custom.conf
          sudo pacman -Syy
          choice=$choice_count
          echo $done_format
-         highlight=4
-      elif [ $choice -eq 5 ] ; then
+         highlight=2
+      elif [ $choice -eq 3 ] ; then
          echo "${bldgreen} ==> Installing main packages${txtrst}"
          ## Installing backups on guest
          ## Install offical packages
          sudo pacman -S --needed $(cat $dotfiles_main)
          choice=$choice_count
          echo $done_format
-         highlight=5
-      elif [ $choice -eq 6 ] ; then
-         echo "${bldgreen} ==> Installing AUR packages${txtrst}"
+         highlight=3
+      elif [ $choice -eq 4 ] ; then
+         echo "${bldgreen} ==> Installing AUR packages (no confirm)${txtrst}"
          ## Install non-official (local) packages
          yaourt --noconfirm -S $(cat $dotfiles_local | grep -vx "$(pacman -Qqm)")
          choice=$choice_count
          echo $done_format
-         highlight=6
-      elif [ $choice -eq 7 ] ; then
+         highlight=4
+      elif [ $choice -eq 5 ] ; then
          echo "${bldgreen} ==> Installing AUR packages${txtrst}"
          ## Install non-official (local) packages
          yaourt -S $(cat $dotfiles_local | grep -vx "$(pacman -Qqm)")
          choice=$choice_count
          echo $done_format
-         highlight=7
-      elif [ $choice -eq 8 ] ; then
+         highlight=5
+      elif [ $choice -eq 6 ] ; then
          echo "${bldgreen} ==> Backing up and copying user configs${txtrst}"
          mv ${my_home}.gmail_symlink ${my_home}.gmail_symlink.bak
          cp ${my_home}${dotfiles}.gmail_symlink ${my_home}.gmail_symlink
@@ -371,8 +348,8 @@ sudo cp ${my_home}${dotfiles}etc/custom.conf /etc/X11/xorg.conf.d/custom.conf
          ls --color=auto -a
          choice=$choice_count
          echo $done_format
-         highlight=8
-      elif [ $choice -eq 9 ] ; then
+         highlight=6
+      elif [ $choice -eq 7 ] ; then
          echo "${bldgreen} ==> Git clone awesomewm-X, luakit-X and conky-X... Installing...${txtrst}"
          packer -S luakit
          mkdir ${my_home}.config
@@ -394,15 +371,15 @@ sudo cp ${my_home}${dotfiles}etc/custom.conf /etc/X11/xorg.conf.d/custom.conf
          ln -sfn ${my_home}.config/awesome/icons/AwesomeLight.png ${my_home}.config/awesome/icons/menu_icon.png
          ln -sfn ${my_home}.config/awesome/themes/current/theme.lua ${my_home}.config/luakit/awesometheme.lua
          cd ${my_home}Development
-         git clone git://github.com/LokiChaos/luakit-plugins.git LokiChaos-luakit-plugins
-         git clone git://github.com/mason-larobina/luakit-plugins.git
-         mkdir ${my_home}.config/luakit/plugins
-         cp -r luakit-plugins/adblock ${my_home}.config/luakit/plugins/adblock
-         cp -r LokiChaos-luakit-plugins/plugins/* ${my_home}.config/luakit/plugins/
+         #git clone git://github.com/LokiChaos/luakit-plugins.git LokiChaos-luakit-plugins
+         #git clone git://github.com/mason-larobina/luakit-plugins.git
+         #mkdir ${my_home}.config/luakit/plugins
+         #cp -r luakit-plugins/adblock ${my_home}.config/luakit/plugins/adblock
+         #cp -r LokiChaos-luakit-plugins/plugins/* ${my_home}.config/luakit/plugins/
+         mkdir ${my_home}.local/share/luakit/adblock
          cd ${my_home}.local/share/luakit/adblock
          wget https://easylist-downloads.adblockplus.org/easylist.txt
-         mkdir ${my_home}.cache
-         mkdir ${my_home}.cache/awesome
+         mkdir -p ${my_home}.cache/awesome
          touch ${my_home}.cache/awesome/stderr
          touch ${my_home}.cache/awesome/stdout
          mkdir ${my_home}.config/conky/arch/.cache
@@ -425,15 +402,15 @@ sudo cp ${my_home}${dotfiles}etc/custom.conf /etc/X11/xorg.conf.d/custom.conf
          ln -sfn ${my_home}.config/awesome/icons/AwesomeLight.png ${my_home}.config/awesome/icons/menu_icon.png
          ln -sfn ${my_home}.config/awesome/themes/current/theme.lua ${my_home}.config/luakit/awesometheme.lua
          cd ${my_home}Development
-         git clone git://github.com/LokiChaos/luakit-plugins.git LokiChaos-luakit-plugins
-         git clone git://github.com/mason-larobina/luakit-plugins.git
-         mkdir ${my_home}.config/luakit/plugins
-         cp -r luakit-plugins/adblock ${my_home}.config/luakit/plugins/adblock
-         cp -r LokiChaos-luakit-plugins/plugins/* ${my_home}.config/luakit/plugins/
+         #git clone git://github.com/LokiChaos/luakit-plugins.git LokiChaos-luakit-plugins
+         #git clone git://github.com/mason-larobina/luakit-plugins.git
+         #mkdir ${my_home}.config/luakit/plugins
+         #cp -r luakit-plugins/adblock ${my_home}.config/luakit/plugins/adblock
+         #cp -r LokiChaos-luakit-plugins/plugins/* ${my_home}.config/luakit/plugins/
+         mkdir ${my_home}.local/share/luakit/adblock
          cd ${my_home}.local/share/luakit/adblock
          wget https://easylist-downloads.adblockplus.org/easylist.txt
-         mkdir ${my_home}.cache
-         mkdir ${my_home}.cache/awesome
+         mkdir -p ${my_home}.cache/awesome
          touch ${my_home}.cache/awesome/stderr
          touch ${my_home}.cache/awesome/stdout
          mkdir ${my_home}.config/conky/arch/.cache
@@ -446,8 +423,8 @@ sudo cp ${my_home}${dotfiles}etc/custom.conf /etc/X11/xorg.conf.d/custom.conf
          ls --color=auto -a
          choice=$choice_count
          echo $done_format
-         highlight=9
-      elif [ $choice -eq 10 ] ; then
+         highlight=7
+      elif [ $choice -eq 8 ] ; then
          echo "${bldgreen} ==> Installing Apache/MySQL/PHP/PHPMyAdmin/mpd/tor/privoxy configuration files${txtrst}"
          sudo cp ${my_home}${dotfiles}etc/mpd.conf /etc/mpd.conf
          ln -s /etc/mpd.conf ${my_home}.mpdconf
@@ -875,8 +852,8 @@ Cool, well we are still root so let's drop down to $USER"
          sleep 3s
          choice=$choice_count
          echo $done_format
-         highlight=10
-      elif [ $choice -eq 11 ] ; then
+         highlight=8
+      elif [ $choice -eq 9 ] ; then
          echo $done_format
          echo "${bldgreen} ==> Installing VirtualBox guest additions...${txtrst}"
          sleep 3s
@@ -918,7 +895,7 @@ ${bldgreen}Log back in as $USER, then type: 'startx'${txtrst}"
          sudo reboot
          sleep 60s
       else
-         echo "${bldred}Please make a choice between 1-11! (HINT: start at 1)${txtrst}"
+         echo "${bldred}Please make a choice between 1-9! (HINT: start at 1)${txtrst}"
          highlight=${highlight}
          menulist
          choice=$choice_count
