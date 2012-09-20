2a3dbaaa7ae370202618133c286319b5c68cce65
pdq <idkbot@gmail.com>
2012-09-20 08:37:20 -0500

    updated packages lists from source

--- php/test.php
+++ php/test.php
@@ -1,3 +1,15 @@
  <?php 
 var_dump($argv);
+// php_beautifier -l "ArrayNested() Pear()" -f ~/Development/U-232-V3/*.php U-232-V3-beautified/
+cln-1.3.2-1  iso-codes-3.38-1  kde-base-artwork-4.9.1-1
+              kdebase-katepart-4.9.1-1  kdebase-workspace-4.9.1-3  libgles-8.0.4-3
+              libqalculate-0.9.7-3  libxklavier-5.2.1-2  libxres-1.0.6-1
+              lm_sensors-3.3.2-4  polkit-kde-0.99.0-2  tidyhtml-1.46-2
+              kde-l10n-en_gb-4.9.1-1  kde-wallpapers-4.9.1-1  kdebase-dolphin-4.9.1-1
+              kdebase-kdepasswd-4.9.1-1  kdebase-kdialog-4.9.1-1
+              kdebase-keditbookmarks-4.9.1-1  kdebase-kfind-4.9.1-1
+              kdebase-konq-plugins-4.9.1-1  kdebase-konqueror-4.9.1-1
+              kdebase-konsole-4.9.1-1  kdebase-kwrite-4.9.1-1  kdebase-plasma-4.9.1-1
+              phonon-vlc-0.6.0-1
+
 ?>
