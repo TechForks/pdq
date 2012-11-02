8f1a9c25f24eb62887575f5fad1c041f237d5894
pdq <idkbot@gmail.com>
2012-11-02 04:21:25 -0500

    updated packages lists from source

--- bin/lamp.sh
+++ bin/lamp.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 case "${1:-''}" in
 'start')
-rc.d start httpd
-rc.d start mysqld
+systemctl start httpd
+systemctl start mysqld
 /usr/bin/memcached -d -m 512 -l 127.0.0.1 -p 11211 -u nobody
 tput bold;
 echo ":: Starting Memcached Daemon                                                [DONE]"
 ;;
 
 'stop')
-rc.d stop httpd
-rc.d stop mysqld
+systemctl stop httpd
+systemctl stop mysqld
 killall memcached
 tput bold;
 echo ":: Stopping Memcached Daemon                                                [DONE]"
