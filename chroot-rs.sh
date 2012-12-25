10fefad80e3d054226c6af25f48e4b3d9430b2f3
pdq <idkbot@gmail.com>
2012-12-25 03:41:52 -0600

    working

--- chroot-rs.sh
+++ chroot-rs.sh
@@ -409,17 +409,17 @@ if [ $(id -u) -eq 0 ]; then
     edit_file() {
         dialog --clear --backtitle "$upper_title" --title "[ Edit files ]" --msgbox "Please choose a file to open with nano.\n\nUse the <tab>, <spacebar> and arrow keys to navigate and select file." 10 30
 
-        file_edit=$(dialog --stdout --backtitle "$upper_title" --title "Please select file." --fselect /etc/ 20 40)
+        file_edit=$(dialog --stdout --backtitle "$upper_title" --title "Please select file." --fselect /etc/ 0 0)
 
         if [ $? = 0 ] ; then
             nano "$file_edit"
-            # dialog --clear --backtitle "$upper_title" --title "[ Edit files ]" --defaultno --yesno "Edit/view another file?" 10 30
-            # if [ $? = 0 ] ; then
-            #     edit_file
-            # else
-            #     chroot_menu
-            #     return 0 
-            # fi
+            dialog --clear --backtitle "$upper_title" --title "[ Edit files ]" --defaultno --yesno "Edit/view another file?" 10 30
+            if [ $? = 0 ] ; then
+                edit_file
+            else
+                chroot_menu
+                return 0 
+            fi
         else
             chroot_menu
             return 0 
