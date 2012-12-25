6abaad10042d48378b5c3e8b0fdc53c099a1c893
pdq <idkbot@gmail.com>
2012-12-24 22:51:42 -0600

    testing

--- reinstaller.sh
+++ reinstaller.sh
@@ -209,6 +209,7 @@ if [ $(id -u) -eq 0 ]; then
             psout=$(cat $TMP/psout)
             mkswap $psout
             swapon $psout
+            dialog --clear --title "SWAP SETUP" --msgbox "Ran: mkswap $psout and swapon $psout" 10 70
         fi
     }
 
@@ -281,8 +282,9 @@ if [ $(id -u) -eq 0 ]; then
             exit_installer
         fi
        
-        dialog --clear --title "$upper_title" --inputbox "Please enter any packages you would like added to the initial base system installation.\n\nSeperate multiple packages with a space.\n\nIf you do not wish to add any packages beyond the default:\nbase base-devel sudo git hub rsync wget\nleave input blank and continue." 10 70 2> $TMP/ppkgs
-        ppkgs=$(cat $TMP/ppkgs)
+        dialog --clear --backtitle "$upper_title" --title "Custom packages" --inputbox "Please enter any packages you would like added to the initial base system installation.\n\nSeperate multiple packages with a space.\n\nIf you do not wish to add any packages beyond the default:\nbase base-devel sudo git hub rsync wget\nleave input blank and continue." 10 70 2> $TMP/ppkgs
+        ppkgs="$(cat $TMP/ppkgs)"
+
         pacstrap /mnt base base-devel sudo git hub rsync wget $ppkgs
         
         dialog --clear --title "$upper_title" --msgbox "Installed base base-devel sudo git hub rsync wget $ppkgs to /mnt.\n\n Hit enter to return to menu" 10 30
