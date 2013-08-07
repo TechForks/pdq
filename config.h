e6d4d910348fdbc261b714f7df2408fdbc60b1c1
pdq <idkbot@gmail.com>
2013-08-07 17:21:11 -0500

    updated pdq to current working copy

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "-*-*-medium-*-*-*-11-*-*-*-*-*-*-*";
+static const char font[]        = "-*-*-medium-*-*-*-12-*-*-*-*-*-*-*";
 static const char normbgcolor[] = "#090909";
 static const char normfgcolor[] = "#FFFFFF";
 static const char selbgcolor[]  = "#090909";
